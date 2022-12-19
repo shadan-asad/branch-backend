55085d2b4a8bf2e7818d0e24880992acfd33ba60
shadan-asad <iamshadanasad@gmail.com>
2022-12-19 10:46:09 +0530

    Created servlet to send user messages to the agent ui

---
+++ src/main/java/controller/MsgServlet.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.UserMsgDao;
+import model.UserMsg;
+
+/**
+ * Servlet implementation class MsgServlet
+ */
+@WebServlet("/")
+public class MsgServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserMsgDao userMsgDao;
+	private Gson gson;
+	private String JSONresponse;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MsgServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        this.userMsgDao = new UserMsgDao();
+        this.gson  = new Gson();
+        this.JSONresponse = null;
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			fetchMsg(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.getWriter().append(JSONresponse);
+		JSONresponse = null;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private void fetchMsg(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		 ArrayList<UserMsg> in = null;
+		 in = userMsgDao.fetchMsg();
+		 JSONresponse = gson.toJson(in);
+	 }
+
+}
